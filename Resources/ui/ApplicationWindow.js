ca253c4e1b4274a9d1d878512c49a146cbd09604
m0hit <mohit.ed+signup@gmail.com>
2012-04-15 18:32:44 -0700

    rewriting camera view as camera window

--- Resources/ui/ApplicationWindow.js
+++ Resources/ui/ApplicationWindow.js
@@ -1,28 +1,22 @@
 // designbook application scope
 var db = {};
 
-
 //Application Window Component Constructor
 function ApplicationWindow() {
-   //load component dependencies
-   var CameraView = require('ui/CameraView');
 
    //create component instance
    var self = Ti.UI.createWindow({
-   	  title: 'Main designbook Window',
+      title: 'Main designbook Window',
       backgroundColor:'#ffffff',
       navBarHidden:true,
       exitOnClose:true
    });
 
-   //construct UI
-   var cameraView = new CameraView(self);
-
-   if (cameraView !=  null) {
-      // if camera is not available
-      // just add the overlay (for testing)
-      self.add(cameraView);
-   }
+   var cameraWindow = Ti.UI.createWindow({
+      url: './CameraWindow.js',
+      title: 'CameraWindow'
+   });
+   cameraWindow.open();
 
    return self;
 }
--- Resources/ui/CameraView.js
+++ Resources/ui/CameraView.js
@@ -1,119 +1,121 @@
-//CameraView Component Constructor
-var CameraView = function(w1in) {
+////CameraView Component Constructor
+//var CameraView = function(win) {
 
-   var navbars = require('/ui/common/navbars');
-   var bottomBar = navbars.createBottomBar(false);
+   //var navbars = require('/ui/common/navbars');
+   //var bottomBar = navbars.createBottomBar(false);
 
-   var topBar = TopBar();
+   //var topBar = TopBar();
 
-   // create the camera overlay
-   var overlay = Ti.UI.createView();
-   overlay.add(bottomBar);
-   overlay.add(topBar);
+   //// create the camera overlay
+   //var overlay = Ti.UI.createView();
+   //overlay.add(bottomBar);
+   //overlay.add(topBar);
 
-   // don't show camera if not present (for simulator testing)'
-   if (Titanium.Media.isCameraSupported == false) {
-      // just render the overlay
-      return overlay;
-   }
+   //// don't show camera if not present (for simulator testing)'
+   //if (Titanium.Media.isCameraSupported == false) {
+      //// just render the overlay
+      //return overlay;
+   //}
 
-   Titanium.Media.showCamera({
-      showControls: false,
-      allowEditing: false,
-      autohide: false,
-      saveToPhotoGallery: true,
-      overlay: overlay,
+   //Titanium.Media.showCamera({
+      //showControls: false,
+      //allowEditing: false,
+      //autohide: false,
+      //saveToPhotoGallery: true,
+      //overlay: overlay,
 
-      success:function(event) {
-         // gets called after a picture is taken
-         // save it, move to next screen
-         Ti.API.info('in success function');
-         // place our picture into our window
-         var imageView = Ti.UI.createImageView({
-            image: event.media,
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0
-         });
+      //success:function(event) {
+         //// gets called after a picture is taken
+         //// save it, move to next screen
+         //Ti.API.info('in success function');
+         //// place our picture into our window
+         //var imageView = Ti.UI.createImageView({
+            //image: event.media,
+            //left: 0,
+            //right: 0,
+            //top: 0,
+            //bottom: 0
+         //});
 
-         var testLabel = Ti.UI.createLabel({
-            width: '100dp',
-            height: '50dp',
-            title: 'TESTING THIS IMAGE',
-            color: 'white',
-         });
-         
-         imageView.add(testLabel);
+         //var testLabel = Ti.UI.createLabel({
+            //width: '100dp',
+            //height: '50dp',
+            //title: 'TESTING THIS IMAGE',
+            //color: 'white',
+         //});
 
-         // programatically hide the camera
-         Ti.Media.hideCamera();
-      },
-      cancel:function(event) {
-         alert("camera cancel!");
-      },
-      error:function(error) {
-         if (error.code == Titanium.Media.NO_CAMERA) {
-            alert("No camera here.")
-         } else {
-            alert("Camera Error: " + error.code);
-         }
-      }
-   });
+         //imageView.add(testLabel);
 
-   return null;
-}
+         //win.add(imageView)
 
-module.exports = CameraView;
+         //// programatically hide the camera
+         //Ti.Media.hideCamera();
+      //},
+      //cancel:function(event) {
+         //alert("camera cancel!");
+      //},
+      //error:function(error) {
+         //if (error.code == Titanium.Media.NO_CAMERA) {
+            //alert("No camera here.")
+         //} else {
+            //alert("Camera Error: " + error.code);
+         //}
+      //}
+   //});
 
-var TopBar = function() {
-	
-   var topBar = Ti.UI.createView({
-      backgroundColor: 'transparent',
-       opacity: 0.7,
-       top: 0,
-       left: 0,
-       right: 0,
-       height: 60,
-       layout: 'horizontal',
-       color: 'white'
-   });
+   //return null;
+//}
 
-   var projectSelector = Ti.UI.createButton({
-      title: 'Project: HACK DAY',
-       backgroundColor: 'black',
-       width: '120dp',
-       height: '30dp',
-       top: '10dp',
-       left: '10dp',
-       style: Titanium.UI.iPhone.SystemButtonStyle.PLAIN,
-       font: { fontSize: 11, font: 'Helvetica'}
-   });
+//module.exports = CameraView;
 
-   projectSelector.addEventListener('click', function(e) {
-      //bring up project selection menu
-      alert("show project list");
-   });
+//var TopBar = function() {
 
-   var stageSelector = Ti.UI.createButton({
-      title: 'Stage: DEFINE',
-       backgroundColor: 'black',
-       width: '100dp',
-       height: '30dp',
-       top: '10dp',
-       left: '10dp',
-       style: Titanium.UI.iPhone.SystemButtonStyle.PLAIN,
-       font: { fontSize: 11, font: 'Helvetica'}
-   });
+   //var topBar = Ti.UI.createView({
+      //backgroundColor: 'transparent',
+       //opacity: 0.7,
+       //top: 0,
+       //left: 0,
+       //right: 0,
+       //height: 60,
+       //layout: 'horizontal',
+       //color: 'white'
+   //});
 
-   stageSelector.addEventListener('click', function(e) {
-      //bring up stage selection menu
-      alert("show design stage list");
-   });
+   //var projectSelector = Ti.UI.createButton({
+      //title: 'Project: HACK DAY',
+       //backgroundColor: 'black',
+       //width: '120dp',
+       //height: '30dp',
+       //top: '10dp',
+       //left: '10dp',
+       //style: Titanium.UI.iPhone.SystemButtonStyle.PLAIN,
+       //font: { fontSize: 11, font: 'Helvetica'}
+   //});
 
-   // add buttons to top bar
-   topBar.add(projectSelector);
-   topBar.add(stageSelector);
+   //projectSelector.addEventListener('click', function(e) {
+      ////bring up project selection menu
+      //alert("show project list");
+   //});
+
+   //var stageSelector = Ti.UI.createButton({
+      //title: 'Stage: DEFINE',
+       //backgroundColor: 'black',
+       //width: '100dp',
+       //height: '30dp',
+       //top: '10dp',
+       //left: '10dp',
+       //style: Titanium.UI.iPhone.SystemButtonStyle.PLAIN,
+       //font: { fontSize: 11, font: 'Helvetica'}
+   //});
+
+   //stageSelector.addEventListener('click', function(e) {
+      ////bring up stage selection menu
+      //alert("show design stage list");
+   //});
+
+   //// add buttons to top bar
+   //topBar.add(projectSelector);
+   //topBar.add(stageSelector);
    
-   return topBar;
-}
+   //return topBar;
+//}
